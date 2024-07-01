741e652a1ed002df38021c4684ef526eefd3ae04
Madu-f0r0 <duherbert@gmail.com>
2024-07-01 21:51:24 +0100

    Modified IP adress request code

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -12,7 +12,7 @@ class HelloView(APIView):
         client_ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR', '127.0.0.1'))
 
         # Get client's location
-        client_location_response = requests.get(f'https://ip-api.com/json/{client_ip}')
+        client_location_response = requests.get(f'http://ip-api.com/json/{client_ip}')
         client_location = client_location_response.json()
         city = client_location.get('city', 'Unknown')
 
@@ -26,6 +26,7 @@ class HelloView(APIView):
         
         return Response({
             'client_ip': client_ip,
+            'location_response': client_location_response,
             'location': city,
             'greeting': greeting
         }, status=status.HTTP_200_OK)
